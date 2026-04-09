00447d151eb35ab6e2d173cbf998c3d4b28be282
githubdemo <gunjansengar444@gmail.com>
2026-04-09 23:14:59 +0530

    Maximum Points You Can Obtain from Cards updated - Leetcode 1423 (Medium)

--- Arrays/MaximumPointsYouCanObtainfromCards1423.java
+++ Arrays/MaximumPointsYouCanObtainfromCards1423.java
@@ -1,18 +1,47 @@
+// class Solution {
+//     public int maxScore(int[] cardPoints, int k) {
+//         int n=cardPoints.length;
+//         int leftSum=0;
+//         for(int i=0;i<k;i++){
+//             leftSum+=cardPoints[i];
+//         }
+//         int maxSum=leftSum;
+//         int rightSum=0;
+//         for(int i=1;i<=k;i++){
+//             leftSum-=cardPoints[k-i];
+//             rightSum+=cardPoints[n-i];
+//             maxSum=Math.max(maxSum,leftSum+rightSum);
+//         }
+//         return maxSum;
+        
+//     }
+//}
+
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
         int n=cardPoints.length;
-        int leftSum=0;
-        for(int i=0;i<k;i++){
-            leftSum+=cardPoints[i];
+        int totalSum=0;
+        for(int nums: cardPoints){
+            totalSum=totalSum+nums;
         }
-        int maxSum=leftSum;
-        int rightSum=0;
-        for(int i=1;i<=k;i++){
-            leftSum-=cardPoints[k-i];
-            rightSum+=cardPoints[n-i];
-            maxSum=Math.max(maxSum,leftSum+rightSum);
+        if(k==n){
+            return totalSum;
         }
-        return maxSum;
-        
+        int windowSize=n-k;
+        int windowSum=0;
+        for(int i=0;i<windowSize;i++){
+            windowSum+=cardPoints[i];
+        }
+        int minSum=windowSum;
+        for(int i=windowSize;i<n;i++){
+            windowSum+=cardPoints[i];
+            windowSum-=cardPoints[i-windowSize];
+            minSum=Math.min(minSum,windowSum);
+        }
+        return totalSum-minSum;
     }
-}
+
+
+
+
+    }

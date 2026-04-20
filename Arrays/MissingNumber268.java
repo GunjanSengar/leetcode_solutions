be240d48b3920953485c9f3350d05637a5a8bf69
GunjanSengar <gunjansengar444@gmail.com>
2026-04-20 10:01:01 +0530

    Missing Numbers - Leetcode 268 (Easy)

---
+++ Arrays/MissingNumber268.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int expectedSum=n*(n+1)/2;
+    int currentSum=0;
+    for(int i=0;i<n;i++){
+        currentSum+=nums[i];
+    }
+    int actualSum=expectedSum-currentSum;
+    return actualSum;    
+    }
+
+}

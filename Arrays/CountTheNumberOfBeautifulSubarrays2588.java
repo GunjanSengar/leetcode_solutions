154915434db0fd87333d44b38da568567481cafe
GunjanSengar <gunjansengar444@gmail.com>
2026-05-19 00:24:25 +0530

    Count the Number of Beautiful Subarrays - Leetcode Problem 2588 (Medium)

---
+++ Arrays/CountTheNumberOfBeautifulSubarrays2588.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long beautifulSubarrays(int[] nums) {
+        Map<Long, Long> map =new HashMap<>();
+        long xor=0;
+        long count =0;
+        map.put(0L,1L);
+        for(int num: nums){
+            xor^=num;
+            count+=map.getOrDefault(xor,0L);
+            map.put(xor, map.getOrDefault(xor,0L)+1);
+
+        }
+        return count;
+        
+    }
+}

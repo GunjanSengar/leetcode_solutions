5a169361177f7fd0d15419bb233371d210a01ae9
Gunjan Sengar <gunjansengar444@gmail.com>
2026-08-23 21:02:20 +0530

    Time: 1 ms (60.95%), Space: 43.5 MB (16.83%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -7,7 +7,7 @@ class Solution {
 
             for (int j = 1; j < strs.length; j++) {
 
-                if (i >= strs[j].length() ||
+                if (i == strs[j].length() ||
                     strs[j].charAt(i) != ch) {
 
                     return strs[0].substring(0, i);
